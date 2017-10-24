2af9ed770f0ab0efd3c9328a8c111128e76eb293
Alyssa Roose <roose.alyssa@gmail.com>
2017-10-24 20:29:21 +0200

    Add package.json and index.js file for the library

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import BoredButton from './bored-button';
+import DatePicker from './bored-button';
+import FlexSelector from './bored-button';
+import TextLine from './bored-button';
+import TimePicker from './bored-button';
+
+export { BoredButton, DatePicker, FlexSelector, TextLine, TimePicker };

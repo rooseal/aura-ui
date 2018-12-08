1fde504c5177bf70ad0c989b6d628d12b5604aa7
Alyssa Roose <roose.alyssa@gmail.com>
2018-12-08 16:31:09 +0100

    Add theme first theme file

---
+++ src/css/themes/teal.js
@@ -0,0 +1,10 @@
+export default {
+  primary: '#008080',
+  primaryLight: '#029696',
+  primaryDark: '#027373',
+  primaryDisabled: '#025c5c',
+  secondary: '#802000',
+  secondaryLight: '#d13400',
+  secondaryDark: '#421100',
+  textPrimary: '#333333',
+};

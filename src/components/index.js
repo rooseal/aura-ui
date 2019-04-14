6164392cea77b82fb895bda7f3c657ffcb7ee7f9
Alyssa Roose <roose.alyssa@gmail.com>
2019-04-14 20:10:16 +0200

    Add anchors, buttons and text comps to barrel file

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import { Button, Submit, StyledButton, Anchor as ButtonAnchor } from './buttons';
+import { Anchor, Button as AnchorButton, StyledAnchor, StyledButtonAnchor } from './anchors';
+import { TextLine, TextArea } from './text';
+
+export {
+  Button,
+  Submit,
+  StyledButton,
+  ButtonAnchor,
+  Anchor,
+  AnchorButton,
+  StyledAnchor,
+  StyledButtonAnchor,
+  TextLine,
+  TextArea
+};

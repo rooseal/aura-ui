4ab0982be5d46f29d160de5eeff65dad56805013
Alyssa Roose <roose.alyssa@gmail.com>
2019-04-20 13:40:45 +0200

    Fix imports for anchors and buttons in stories

--- src/components/buttons/buttons.stories.jsx
+++ src/components/buttons/buttons.stories.jsx
@@ -2,10 +2,10 @@
 
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { StyledButton, Button, Submit, Anchor } from '.';
+import { StyledButton, Button, Submit, AnchorButton } from '.';
 
 storiesOf('Buttons', module)
   .add('Styled Base', () => <StyledButton>Styled Base</StyledButton>)
   .add('Default Button', () => <Button>Default Button</Button>)
   .add('Default Submit', () => <Submit>Default Submit</Submit>)
-  .add('Default Anchor', () => <Anchor>Default Anchor</Anchor>);
+  .add('Default Anchor', () => <AnchorButton>Default Anchor</AnchorButton>);
